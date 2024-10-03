a4834da45f1dda7b4fbdc1f26aec38425cf6f0b6
Caleb Porzio <calebporzio@gmail.com>
2024-10-03 15:24:29 -0400

    Fix button group with dropdown buttons

--- stubs/resources/views/flux/button/group.blade.php
+++ stubs/resources/views/flux/button/group.blade.php
@@ -5,6 +5,10 @@ $classes = Flux::classes()
         '[&>[data-flux-group-target]:not(:first-child):not(:last-child)]:rounded-none',
         '[&>[data-flux-group-target]:first-child:not(:last-child)]:rounded-r-none',
         '[&>[data-flux-group-target]:last-child:not(:first-child)]:rounded-l-none',
+
+        '[&>*:not(:first-child):not(:last-child):not(:only-child)_[data-flux-group-target]]:rounded-none',
+        '[&>*:first-child:not(:last-child)_[data-flux-group-target]]:rounded-r-none',
+        '[&>*:last-child:not(:first-child)_[data-flux-group-target]]:rounded-l-none',
     ])
     ;
 @endphp
