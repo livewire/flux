0649e7b63fb7259456d62a5cc24effe4cfb70c92
Daniel Rona <daniel@rona.email>
2024-10-16 14:54:35 +0200

    blade-ui-kit icon support with buk::icon-name

--- stubs/resources/views/flux/icon/index.blade.php
+++ stubs/resources/views/flux/icon/index.blade.php
@@ -1,12 +1,26 @@
 {{-- Credit: Heroicons (https://heroicons.com) --}}
+{{-- Credit: Blade-Ui-Kit (https://blade-ui-kit.com/) --}}
 
 @props([
     'icon' => null,
     'name' => null,
+    'variant' => 'outline',
+    'classes' => null,
 ])
 
 @php
-$icon = $name ?? $icon;
+    $icon = $name ?? $icon;
+    $classes = Flux::classes('shrink-0')
+        ->add(match($variant) {
+            'outline' => '[:where(&)]:size-6',
+            'solid' => '[:where(&)]:size-6',
+            'mini' => '[:where(&)]:size-5',
+            'micro' => '[:where(&)]:size-4',
+        });
 @endphp
 
-<x-dynamic-component :component="'flux::icon.' . $icon" :$attributes />
+<x-dynamic-component :component="str_starts_with($icon,'buk::') ? str_replace('buk::', '', $icon) : 'flux::icon.' . $icon"
+                     :attributes="$attributes->except('classes')"
+                     :class="$classes"
+                     :variant="$variant"
+/>
