806e710f774b8245b93a2df36d39d78f0f7f83b3
Caleb Porzio <calebporzio@gmail.com>
2024-12-12 16:04:00 -0500

    wip

---
+++ stubs/resources/views/flux/dark-mode-beacon.blade.php
@@ -0,0 +1,4 @@
+<!-- This file's sole purpose is to get compiled by Tailwind and allow us to detect the -->
+<!-- user's darkMode setting by inspecting the effects of using this selector elsewhere... -->
+
+dark:[&[data-flux-dark-mode-beacon]]:hidden
