c705f6ad1a1707afbafdae576f8725d587a6d08a
Josh Hanley <josh@hitsystems.com.au>
2025-01-20 18:52:19 +1100

    Revert automated changes

--- stubs/resources/views/flux/menu/index.blade.php
+++ stubs/resources/views/flux/menu/index.blade.php
@@ -1,10 +1,10 @@
 @php
 $classes = Flux::classes()
     ->add('[:where(&)]:min-w-48 p-[.3125rem]')
-    ->add('rounded-lg shadow-xs')
+    ->add('rounded-lg shadow-sm')
     ->add('border border-zinc-200 dark:border-zinc-600')
     ->add('bg-white dark:bg-zinc-700')
-    ->add('focus:outline-hidden')
+    ->add('focus:outline-none')
     ;
 @endphp
 
