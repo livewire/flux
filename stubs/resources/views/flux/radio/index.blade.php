1e04952399eb821e0ed59bce8aa1be8b01b7a2ac
Josh Hanley <josh@hitsystems.com.au>
2025-02-20 08:24:55 +1100

    Fix checkbox and radio wrapping element variant does not exist error

--- stubs/resources/views/flux/radio/index.blade.php
+++ stubs/resources/views/flux/radio/index.blade.php
@@ -4,4 +4,11 @@
     'variant' => 'default',
 ])
 
+@php
+// This prevents variants picked up by `@aware()` from other wrapping components like flux::modal from being used here...
+$variant = $variant !== 'default' && Flux::componentExists('radio.variants.' . $variant)
+    ? $variant
+    : 'default';
+@endphp
+
 <flux:delegate-component :component="'radio.variants.' . $variant">{{ $slot }}</flux:delegate-component>
