f965af9ea48f13b93ef40a287ed42bc74a327f8a
Harry Harrison <h@hrrsn.dev>
2025-03-03 09:15:01 +0000

    Add the selected prop to select options

--- stubs/resources/views/flux/select/option/variants/default.blade.php
+++ stubs/resources/views/flux/select/option/variants/default.blade.php
@@ -1,9 +1,11 @@
 @props([
     'value' => null,
+    'selected' => null,
 ])
 
 <option
     {{ $attributes }}
     @if (isset($value)) value="{{ $value }}" @endif
     @if (isset($value)) wire:key="{{ $value }}" @endif
+    @if ($selected) selected @endif
 >{{ $slot }}</option>
