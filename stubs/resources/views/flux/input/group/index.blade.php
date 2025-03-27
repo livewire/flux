a28168c346ea345304061204df286740943e3b75
Josh Hanley <josh@hitsystems.com.au>
2025-03-27 13:51:27 +1100

    wip

--- stubs/resources/views/flux/input/group/index.blade.php
+++ stubs/resources/views/flux/input/group/index.blade.php
@@ -9,36 +9,23 @@ $classes = Flux::classes()
     ->add([
         // With the external borders, let's always make sure the first and last children have outside borders.
         // For internal borders, we will ensure that all left borders are removed, but the right borders remain.
-        // That way we shouldn't ever have a double border.
-        // But when there is a prefix or a suffix, then there should be no internal borders.
+        // But when there is a input groupsuffix, then there should be no right internal border.
+        // That way we shouldn't ever have a double border...
 
-        // All inputs
-        '[&>[data-flux-input]:not(:first-child):not(:last-child)>[data-flux-group-target]:not([data-invalid])]:border-x-0',
-        '[&>[data-flux-input]:first-child:not(:last-child)>[data-flux-group-target]:not([data-invalid])]:border-r-0',
+        // All inputs borders...
         '[&>[data-flux-input]:last-child:not(:first-child)>[data-flux-group-target]:not([data-invalid])]:border-l-0',
-        // All selects and date pickers
-        '[&>*:not(:first-child):not(:last-child)>[data-flux-group-target]:not([data-invalid])]:border-x-0',
-        '[&>*:first-child:not(:last-child)>[data-flux-group-target]:not([data-invalid])]:border-r-0',
+        '[&>[data-flux-input]:not(:first-child):not(:last-child)>[data-flux-group-target]:not([data-invalid])]:border-l-0',
+        '[&>[data-flux-input]:has(+[data-flux-input-group-suffix])>[data-flux-group-target]:not([data-invalid])]:border-r-0',
+
+        // Selects and date pickers borders...
         '[&>*:last-child:not(:first-child)>[data-flux-group-target]:not([data-invalid])]:border-l-0',
-        // All buttons
-        '[&>[data-flux-group-target]:not(:first-child):not(:last-child)]:border-x-0',
-        '[&>[data-flux-group-target]:first-child:not(:last-child)]:border-r-0',
+        '[&>*:not(:first-child):not(:last-child)>[data-flux-group-target]:not([data-invalid])]:border-l-0',
+        '[&>*:has(+[data-flux-input-group-suffix])>[data-flux-group-target]:not([data-invalid])]:border-r-0',
+
+        // Buttons borders...
         '[&>[data-flux-group-target]:last-child:not(:first-child)]:border-l-0',
-
-
-
-        // Inputs
-        //'[&>[data-flux-input]:not(:first-child)>[data-flux-group-target]:not([data-invalid])]:border-l-0',
-        // Selects and Date pickers
-        //'[&>*:not(:first-child):not(:only-child)_[data-flux-group-target]:not([data-invalid])]:border-l-0',
-        // Buttons
-        //'[&>[data-flux-group-target]:not(:first-child)]:border-l-0',
-
-        // Testings
-        //'[&>*:has(+[data-flux-input-group-suffix])]:border-r-0',
-
-        // Prevent sandwiched selects (next to inputs) from causing double borders...
-        '[&>*:not(:first-child):not(:last-child):not(:only-child)_[data-flux-group-target]:not([data-invalid])]:border-l-0',
+        '[&>[data-flux-group-target]:not(:first-child):not(:last-child)]:border-l-0',
+        '[&>[data-flux-group-target]:has(+[data-flux-input-group-suffix])]:border-r-0',
 
         // "Weld" the borders of inputs together by overriding their border radiuses...
         '[&>[data-flux-group-target]:not(:first-child):not(:last-child)]:rounded-none',
