37a6456fa25c9353348b98631d64be48af90410b
John Koster <john@stillat.com>
2025-04-26 19:25:35 -0500

    Yeet all this
    Thought of a better way to do this

--- src/Compiler/ComponentCompiler.php
+++ src/Compiler/ComponentCompiler.php
@@ -3,13 +3,12 @@
 namespace Flux\Compiler;
 
 use Flux\Flux;
-use Flux\Support\StrUtil;
 use Illuminate\Support\Str;
 use Illuminate\View\Compilers\ComponentTagCompiler;
 
 class ComponentCompiler extends ComponentTagCompiler
 {
-    protected $hoistedContent = '';
+    protected $isOptimized = false;
 
     public function isFluxComponent($value)
     {
@@ -18,71 +17,11 @@ class ComponentCompiler extends ComponentTagCompiler
 
     protected function compileOptimizedComponent($value)
     {
-        $this->hoistedContent = '';
+        $this->isOptimized = true;
 
-        $value = StrUtil::normalizeLineEndings($value);
-
-        // If the Flux component contains @aware or @props, we need
-        // to make sure we insert our @uncached directive pair
-        // after them. Otherwise, we will be missing info
-        // later on, which will lead to state errors
-        //
-        // This error presents itself as "undefined array key index ''"
         $value = ltrim(preg_replace('/(?<!@)@optimized/', '', $value));
-        preg_match_all('/(?<!@)@(props|aware)\(/', $value, $matches, PREG_OFFSET_CAPTURE);
 
-        if (count($matches[1] ?? []) === 0) {
-            return "<?php Flux::shouldOptimize(); ?>\n@uncached\n$value\n@enduncached";
-        }
-
-        $matches = $matches[1];
-
-        // Get the offset of the first interesting directive.
-        $firstMatchOffset = $matches[0][1];
-
-        // Find anything interesting we may want to hoist
-        $this->hoistedContent = (string) str($value)
-            ->substr(0, $firstMatchOffset - 1)
-            ->trim();
-
-        // Get the offset of the last directive we are interested in
-        $lastMatchOffset = $matches[array_key_last($matches)][1];
-
-        $startLine = str($value)
-            ->substr(0, $lastMatchOffset) // Limit the search space to the start of the last important directive.
-            ->substrCount("\n"); // Find the 0-indexed line number of the directive.
-
-        // Now that we have the line our directive starts on, we need to find the ending of it.
-        $lines = explode("\n", $value);
-        $endLine = null;
-
-        for ($i = $startLine; $i < count($lines); $i++) {
-            $line = (string) str($lines[$i])
-                ->squish(); // Collapse the whitespace to make finding our ending easier.
-
-            if (Str::endsWith($line, ['])', '] )'])) {
-                $endLine = $i;
-                break;
-            }
-        }
-
-        if ($endLine === null) {
-            return $value;
-        }
-
-        // Insert the beginning of our uncached directive.
-        array_splice(
-            $lines,
-            $endLine, 1,
-            [ $lines[$endLine], '@uncached' ]
-        );
-
-        $lines[] = '@enduncached';
-
-        // Add our shouldOptimize call.
-        array_unshift($lines, '<?php Flux::shouldOptimize(); ?>');
-
-        return implode("\n", $lines);
+        return "<?php Flux::shouldOptimize(); ?>\n@uncached\n$value\n@enduncached";
     }
 
     public function compile($value)
@@ -100,8 +39,11 @@ class ComponentCompiler extends ComponentTagCompiler
         }
 
         $value = preg_replace('/(?<!@)@cached/', '<?php Flux::shouldCache(); ?>', $value);
-        $value = preg_replace('/(?<!@)@props\(/', '@fluxProps(', $value);
-        $value = preg_replace('/(?<!@)@aware\(/', '@fluxAware(', $value);
+
+        if (! $this->isOptimized) {
+            $value = preg_replace('/(?<!@)@props\(/', '@fluxProps(', $value);
+            $value = preg_replace('/(?<!@)@aware\(/', '@fluxAware(', $value);
+        }
 
         $value = $this->compileUncachedComponent($value);
         $value = $this->compileUncachedDirective($value);
@@ -119,45 +61,12 @@ class ComponentCompiler extends ComponentTagCompiler
             $compiledExclude = "\Flux\Flux::cache()->exclude({$excludeExpression});";
         }
 
-        $component = Flux::cache()->currentComponent();
-
         return <<<PHP
 <?php
     $compiledExclude
     \Flux\Flux::cache()->addSwap('$replacement', function (\$data) {
         extract(\$data);
         \$__env = \$__env ?? view();
-        \$attributes ??= new \\Illuminate\\View\\ComponentAttributeBag;
-
-?>{$this->hoistedContent}<?php
-
-        \$__newAttributes = [];
-        [\$__propNames, \$__originalPropValues] = \Flux\Flux::cache()->componentProps('$component');
-        
-        foreach (\$attributes->all() as \$__key => \$__value) {
-            if (in_array(\$__key, \$__propNames)) {
-                \$\$__key = \$\$__key ?? \$__value;
-            } else {
-                \$__newAttributes[\$__key] = \$__value;
-            }
-        }
-        
-        \$attributes = new \Illuminate\View\ComponentAttributeBag(\$__newAttributes);
-        unset(\$__propNames);
-        unset(\$__newAttributes);
-        
-        foreach (array_filter(\$__originalPropValues, 'is_string', ARRAY_FILTER_USE_KEY) as \$__key => \$__value) {
-            \$\$__key = \$\$__key ?? \$__value;
-        }
-        unset(\$__originalPropValues);       
-        
-        \$__defined_vars = get_defined_vars();
-        
-        foreach (\$attributes->all() as \$__key => \$__value) {
-            if (array_key_exists(\$__key, \$__defined_vars)) unset(\$\$__key);
-        }
-        
-        unset(\$__defined_vars);
         ob_start();
 ?>$content<?php
         return ob_get_clean();
--- src/Compiler/FluxComponentDirectives.php
+++ src/Compiler/FluxComponentDirectives.php
@@ -8,6 +8,11 @@ use Illuminate\View\AnonymousComponent;
 
 class FluxComponentDirectives
 {
+    public static function normalizeLineEndings($value)
+    {
+        return str_replace(["\r\n", "\r"], "\n", $value);
+    }
+
     public static function compileFluxComponentClass($expression)
     {
         [$component, $alias, $data] = str_contains($expression, ',')
@@ -47,7 +52,7 @@ class FluxComponentDirectives
     {
         $hash = ComponentHashes::popHash();
 
-        return StrUtil::normalizeLineEndings(<<<PHP
+        return static::normalizeLineEndings(<<<PHP
 <?php
     if (isset(\$__fluxHoistedComponentData)) { \$__fluxHoistedComponentDataOriginal{$hash} = \$__fluxHoistedComponentData; } /* Preserve data in the event someone is nesting the same component over, and over, and over. */
     if (isset(\$__fluxCacheKey{$hash})) { \$__fluxCacheKeyOriginal{$hash} = \$__fluxCacheKey{$hash}; }
@@ -104,7 +109,7 @@ PHP);
 
     public static function compileFluxAware($expression)
     {
-        return StrUtil::normalizeLineEndings("<?php foreach ({$expression} as \$__key => \$__value) {
+        return static::normalizeLineEndings("<?php foreach ({$expression} as \$__key => \$__value) {
     \$__consumeVariable = is_string(\$__key) ? \$__key : \$__value;
     if (is_string (\$__key)) {
         \$\$__consumeVariable = \$__env->getConsumableComponentData(\$__key, \$__value);
