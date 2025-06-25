0b5153dbefba85bbfb09ceb6b7b25c9e439f50d0
Sergio Peris <dev.sertxu@gmail.com>
2025-06-25 18:14:57 +0200

    fix padding

--- stubs/resources/views/flux/button/index.blade.php
+++ stubs/resources/views/flux/button/index.blade.php
@@ -62,7 +62,7 @@ $classes = Flux::classes()
     ->add('relative items-center font-medium justify-center gap-2 whitespace-nowrap')
     ->add('disabled:opacity-75 dark:disabled:opacity-75 disabled:cursor-default disabled:pointer-events-none')
     ->add(match ($size) { // Size...
-        'base' => 'h-10 text-sm rounded-lg' . ' ' . ($square ? 'w-10' : 'px-4 [&:has(>:not(span):first-child)]:ps-3 [&:has(>:not(span):last-child)]:pe-3'),
+        'base' => 'h-10 text-sm rounded-lg' . ' ' . ($square ? 'w-10' : 'px-4 [&:has(>:not(span):not([data-flux-loading-indicator]):first-child)]:ps-3 [&:has(>:not(span):not([data-flux-loading-indicator]):last-child)]:pe-3'),
         'sm' => 'h-8 text-sm rounded-md' . ' ' . ($square ? 'w-8' : 'px-3'),
         'xs' => 'h-6 text-xs rounded-md' . ' ' . ($square ? 'w-6' : 'px-2'),
     })
