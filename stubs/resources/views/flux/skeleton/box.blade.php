a165ce2c8d9e2bd6f0df655cdaf48684766a7d56
Caleb Porzio <calebporzio@gmail.com>
2025-11-15 09:55:02 -0500

    wip

---
+++ stubs/resources/views/flux/skeleton/box.blade.php
@@ -0,0 +1,5 @@
+@blaze
+
+<div {{ $attributes->class('[:where(&)]:size-10 [:where(&)]:rounded [:where(&)]:bg-zinc-400/20') }} data-flux-skeleton-box>
+    {{ $slot }}
+</div>
---
+++ stubs/resources/views/flux/skeleton/circle.blade.php
@@ -0,0 +1,5 @@
+@blaze
+
+<div {{ $attributes->class('[:where(&)]:size-10 [:where(&)]:rounded-full [:where(&)]:bg-zinc-400/20') }} data-flux-skeleton-circle>
+    {{ $slot }}
+</div>
