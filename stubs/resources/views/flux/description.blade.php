99d92887fbb2eb5e0ac9f267645a312137951a2d
Caleb Porzio <calebporzio@gmail.com>
2025-11-23 16:33:40 -0500

    add sr-only to description

--- stubs/resources/views/flux/description.blade.php
+++ stubs/resources/views/flux/description.blade.php
@@ -1,5 +1,18 @@
 @blaze
 
-<ui-description {{ $attributes->class('text-sm text-zinc-500 dark:text-white/60') }} data-flux-description>
+@php $srOnly = $srOnly ??= $attributes->pluck('sr-only'); @endphp
+
+@props([
+    'srOnly' => null,
+])
+
+@php
+$classes = Flux::classes()
+    ->add('text-sm text-zinc-500 dark:text-white/60')
+    ->add($srOnly ? 'sr-only' : '')
+    ;
+@endphp
+
+<ui-description {{ $attributes->class($classes) }} data-flux-description>
     {{ $slot }}
 </ui-description>
