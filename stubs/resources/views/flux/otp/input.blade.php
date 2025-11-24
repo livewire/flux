960a215b915a339ad1aca51adce8a5d99e9d06fb
Caleb Porzio <calebporzio@gmail.com>
2025-11-24 15:16:30 -0500

    wip

--- stubs/resources/views/flux/otp/input.blade.php
+++ stubs/resources/views/flux/otp/input.blade.php
@@ -3,7 +3,7 @@
 @php
     $attributes = $attributes
         ->merge([
-            'class' => 'w-8! grow-0 has-focus-within:z-10'
+            'class' => 'w-8! grow-0 has-focus-within:z-10',
             'class:input' => 'px-0! py-3 text-center disabled:opacity-75 disabled:shadow-xs! dark:disabled:shadow-none!',
         ])
         ->merge(['data-flux-otp-input' => ''])
