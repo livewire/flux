d482f782a98ae5c1df2a564ddb98309bb2237e30
Josh Hanley <josh@hitsystems.com.au>
2026-02-17 13:42:36 +1000

    Fix button group styling with tooltip wrappers

--- stubs/resources/views/flux/input/group/index.blade.php
+++ stubs/resources/views/flux/input/group/index.blade.php
@@ -41,6 +41,15 @@ $classes = Flux::classes()
         '[&>*:not(:first-child):not(:last-child):not(:only-child)>[data-flux-input]>[data-flux-group-target]]:rounded-none',
         '[&>*:first-child:not(:last-child)>[data-flux-input]>[data-flux-group-target]]:rounded-e-none',
         '[&>*:last-child:not(:first-child)>[data-flux-input]>[data-flux-group-target]]:rounded-s-none',
+
+        // "Weld" borders for sub-children wrapped in tooltips (button inside tooltip inside modal trigger, etc.)...
+        '[&>*:not(:first-child):not(:last-child):not(:only-child)>[data-flux-tooltip]>[data-flux-group-target]]:rounded-none',
+        '[&>*:first-child:not(:last-child)>[data-flux-tooltip]>[data-flux-group-target]]:rounded-e-none',
+        '[&>*:last-child:not(:first-child)>[data-flux-tooltip]>[data-flux-group-target]]:rounded-s-none',
+
+        // Borders for sub-children wrapped in tooltips...
+        '[&>*:last-child:not(:first-child)>[data-flux-tooltip]>[data-flux-group-target]:not([data-invalid])]:border-s-0',
+        '[&>*:not(:first-child):not(:last-child)>[data-flux-tooltip]>[data-flux-group-target]:not([data-invalid])]:border-s-0',
     ])
     ;
 @endphp
