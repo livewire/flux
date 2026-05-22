459fd348db716fa72fc3942a704f5bcc1c621706
ghabriel <rihulfaakbar@gmail.com>
2026-05-22 09:55:56 +0700

    Replace blade directive with php syntax

--- stubs/resources/views/flux/pagination.blade.php
+++ stubs/resources/views/flux/pagination.blade.php
@@ -22,123 +22,123 @@ $scrollIntoViewJsSnippet = ($scrollTo !== null && $scrollTo !== false)
     : '';
 @endphp
 
-@if ($simple)
+<?php if ($simple): ?>
     <div {{ $attributes->class('pt-3 border-t border-zinc-100 dark:border-zinc-700 flex justify-between items-center') }} data-flux-pagination>
         <div></div>
 
-        @if ($paginator->hasPages())
+        <?php if ($paginator->hasPages()): ?>
             <div class="flex items-center bg-white border border-zinc-200 rounded-[8px] p-[1px] dark:bg-white/10 dark:border-white/10">
-                @if ($paginator->onFirstPage())
+                <?php if ($paginator->onFirstPage()): ?>
                     <div class="flex justify-center items-center size-8 sm:size-6 rounded-[6px] text-zinc-300 dark:text-zinc-500">
                         <flux:icon.chevron-left variant="micro" class="rtl:hidden" />
                         <flux:icon.chevron-right variant="micro" class="hidden rtl:inline" />
                     </div>
-                @else
-                    @if(method_exists($paginator,'getCursorName'))
+                <?php else: ?>
+                    <?php if (method_exists($paginator,'getCursorName')): ?>
                         <button type="button" wire:key="cursor-{{ $paginator->getCursorName() }}-{{ $paginator->previousCursor()->encode() }}" wire:click="setPage('{{$paginator->previousCursor()->encode()}}','{{ $paginator->getCursorName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" class="flex justify-center items-center size-8 sm:size-6 rounded-[6px] text-zinc-400 dark:text-white hover:bg-zinc-100 dark:hover:bg-white/20 hover:text-zinc-800 dark:hover:text-white">
                             <flux:icon.chevron-left variant="micro" class="rtl:hidden" />
                             <flux:icon.chevron-right variant="micro" class="hidden rtl:inline" />
                         </button>
-                    @else
+                    <?php else: ?>
                         <button type="button" wire:click="previousPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" class="flex justify-center items-center size-8 sm:size-6 rounded-[6px] text-zinc-400 dark:text-white hover:bg-zinc-100 dark:hover:bg-white/20 hover:text-zinc-800 dark:hover:text-white">
                             <flux:icon.chevron-left variant="micro" class="rtl:hidden" />
                             <flux:icon.chevron-right variant="micro" class="hidden rtl:inline" />
                         </button>
-                    @endif
-                @endif
+                    <?php endif; ?>
+                <?php endif; ?>
 
-                @if ($paginator->hasMorePages())
-                    @if(method_exists($paginator,'getCursorName'))
+                <?php if (($paginator->hasMorePages())): ?>
+                    <?php if ((method_exists($paginator, 'getCursorName'))): ?>
                         <button type="button" wire:key="cursor-{{ $paginator->getCursorName() }}-{{ $paginator->nextCursor()->encode() }}" wire:click="setPage('{{$paginator->nextCursor()->encode()}}','{{ $paginator->getCursorName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" class="flex justify-center items-center size-8 sm:size-6 rounded-[6px] text-zinc-400 dark:text-white hover:bg-zinc-100 dark:hover:bg-white/20 hover:text-zinc-800 dark:hover:text-white">
                             <flux:icon.chevron-right variant="micro" class="rtl:hidden" />
                             <flux:icon.chevron-left variant="micro" class="hidden rtl:inline" />
                         </button>
-                    @else
+                    <?php else: ?>
                         <button type="button" wire:click="nextPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" class="flex justify-center items-center size-8 sm:size-6 rounded-[6px] text-zinc-400 dark:text-white hover:bg-zinc-100 dark:hover:bg-white/20 hover:text-zinc-800 dark:hover:text-white">
                             <flux:icon.chevron-right variant="micro" class="rtl:hidden" />
                             <flux:icon.chevron-left variant="micro" class="hidden rtl:inline" />
                         </button>
-                    @endif
-                @else
+                    <?php endif; ?>
+                <?php else: ?>
                     <div class="flex justify-center items-center size-8 sm:size-6 rounded-[6px] text-zinc-300 dark:text-zinc-500">
                         <flux:icon.chevron-right variant="micro" class="rtl:hidden" />
                         <flux:icon.chevron-left variant="micro" class="hidden rtl:inline" />
                     </div>
-                @endif
+                <?php endif; ?>
             </div>
-        @endif
+        <?php endif; ?>
     </div>
-@else
+<?php else: ?>
     <div {{ $attributes->class('@container pt-3 border-t border-zinc-100 dark:border-zinc-700 flex justify-between items-center gap-3') }} data-flux-pagination>
-        @if ($paginator->total() > 0)
+        <?php if (($paginator->total() > 0)): ?>
             <div class="text-zinc-500 dark:text-zinc-400 text-xs font-medium whitespace-nowrap">
                 {!! __('Showing') !!} {{ $paginator->firstItem() }} {!! __('to') !!} {{ $paginator->lastItem() }} {!! __('of') !!} {{ $paginator->total() }} {!! __('results') !!}
             </div>
-        @else
+        <?php else: ?>
             <div></div>
-        @endif
+        <?php endif; ?>
 
-        @if ($paginator->hasPages())
+        <?php if (($paginator->hasPages())): ?>
             {{-- Mobile pagination --}}
             <div class="flex @[40rem]:hidden items-center bg-white border border-zinc-200 rounded-[8px] p-[1px] dark:bg-white/10 dark:border-white/10">
-                @if ($paginator->onFirstPage())
+                <?php if (($paginator->onFirstPage())): ?>
                     <div aria-disabled="true" aria-label="{{ __('pagination.previous') }}" class="flex justify-center items-center size-8 sm:size-6 rounded-[6px] text-zinc-300 dark:text-zinc-500">
                         <flux:icon.chevron-left variant="micro" class="rtl:hidden" />
                         <flux:icon.chevron-right variant="micro" class="hidden rtl:inline" />
                     </div>
-                @else
+                <?php else: ?>
                     <button type="button" wire:click="previousPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" aria-label="{{ __('pagination.previous') }}" class="flex justify-center items-center size-8 sm:size-6 rounded-[6px] text-zinc-400 dark:text-zinc-400 hover:bg-zinc-100 dark:hover:bg-white/20 hover:text-zinc-800 dark:hover:text-white">
                         <flux:icon.chevron-left variant="micro" class="rtl:hidden" />
                         <flux:icon.chevron-right variant="micro" class="hidden rtl:inline" />
                     </button>
-                @endif
+                <?php endif; ?>
 
-                @if ($paginator->hasMorePages())
+                <?php if (($paginator->hasMorePages())): ?>
                     <button type="button" wire:click="nextPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" aria-label="{{ __('pagination.next') }}" class="flex justify-center items-center size-8 sm:size-6 rounded-[6px] text-zinc-400 dark:text-zinc-400 hover:bg-zinc-100 dark:hover:bg-white/20 hover:text-zinc-800 dark:hover:text-white">
                         <flux:icon.chevron-right variant="micro" class="rtl:hidden" />
                         <flux:icon.chevron-left variant="micro" class="hidden rtl:inline" />
                     </button>
-                @else
+                <?php else: ?>
                     <div aria-label="{{ __('pagination.next') }}" class="flex justify-center items-center size-8 sm:size-6 rounded-[6px] text-zinc-300 dark:text-zinc-500">
                         <flux:icon.chevron-right variant="micro" class="rtl:hidden" />
                         <flux:icon.chevron-left variant="micro" class="hidden rtl:inline" />
                     </div>
-                @endif
+                <?php endif; ?>
             </div>
 
             {{-- Desktop pagination --}}
             <div class="hidden @[40rem]:flex items-center bg-white border border-zinc-200 rounded-[8px] p-[1px] dark:bg-white/10 dark:border-white/10">
-                @if ($paginator->onFirstPage())
+                <?php if ($paginator->onFirstPage()): ?>
                     <div aria-disabled="true" aria-label="{{ __('pagination.previous') }}" class="flex justify-center items-center size-6 rounded-[6px] text-zinc-300 dark:text-zinc-500">
                         <flux:icon.chevron-left variant="micro" class="rtl:hidden" />
                         <flux:icon.chevron-right variant="micro" class="hidden rtl:inline" />
                     </div>
-                @else
+                <?php else: ?>
                     <button type="button" wire:click="previousPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" aria-label="{{ __('pagination.previous') }}" class="flex justify-center items-center size-6 rounded-[6px] text-zinc-400 dark:text-zinc-400 hover:bg-zinc-100 dark:hover:bg-white/20 hover:text-zinc-800 dark:hover:text-white">
                         <flux:icon.chevron-left variant="micro" class="rtl:hidden" />
                         <flux:icon.chevron-right variant="micro" class="hidden rtl:inline" />
                     </button>
-                @endif
+                <?php endif; ?>
 
-                @foreach (\Livewire\invade($paginator)->elements() as $element)
+                <?php foreach (\Livewire\invade($paginator)->elements() as $element): ?>
                     {{-- "Three Dots" Separator --}}
-                    @if (is_string($element))
+                    <?php if (is_string($element)): ?>
                         <div
                             aria-disabled="true"
                             class="cursor-default flex justify-center items-center text-xs size-6 rounded-[6px] font-medium dark:text-zinc-400 text-zinc-400"
                         >{{ $element }}</div>
-                    @endif
+                    <?php endif; ?>
 
                     {{-- Array Of Links --}}
-                    @if (is_array($element))
-                        @foreach ($element as $page => $url)
-                            @if ($page == $paginator->currentPage())
+                    <?php if (is_array($element)): ?>
+                        <?php foreach ($element as $page => $url): ?>
+                            <?php if ($page == $paginator->currentPage()): ?>
                                 <div
                                     wire:key="paginator-{{ $paginator->getPageName() }}-page{{ $page }}"
                                     aria-current="page"
                                     class="cursor-default flex justify-center items-center text-xs h-6 px-2 rounded-[6px] font-medium dark:text-white text-zinc-800"
                                 >{{ $page }}</div>
-                            @else
+                            <?php else: ?>
                                 <button
                                     wire:key="paginator-{{ $paginator->getPageName() }}-page{{ $page }}"
                                     wire:click="gotoPage({{ $page }}, '{{ $paginator->getPageName() }}')"
@@ -146,23 +146,23 @@ $scrollIntoViewJsSnippet = ($scrollTo !== null && $scrollTo !== false)
                                     type="button"
                                     class="text-xs h-6 px-2 rounded-[6px] text-zinc-400 font-medium dark:text-zinc-400 hover:bg-zinc-100 dark:hover:bg-white/20 hover:text-zinc-800 dark:hover:text-white"
                                 >{{ $page }}</button>
-                            @endif
-                        @endforeach
-                    @endif
-                @endforeach
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                <?php endforeach; ?>
 
-                @if ($paginator->hasMorePages())
+                <?php if ($paginator->hasMorePages()): ?>
                     <button type="button" wire:click="nextPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" aria-label="{{ __('pagination.next') }}" class="flex justify-center items-center size-6 rounded-[6px] text-zinc-400 dark:text-zinc-400 hover:bg-zinc-100 dark:hover:bg-white/20 hover:text-zinc-800 dark:hover:text-white">
                         <flux:icon.chevron-right variant="micro" class="rtl:hidden" />
                         <flux:icon.chevron-left variant="micro" class="hidden rtl:inline" />
                     </button>
-                @else
+                <?php else: ?>
                     <div aria-label="{{ __('pagination.next') }}" class="flex justify-center items-center size-6 rounded-[6px] text-zinc-300 dark:text-zinc-500">
                         <flux:icon.chevron-right variant="micro" class="rtl:hidden" />
                         <flux:icon.chevron-left variant="micro" class="hidden rtl:inline" />
                     </div>
-                @endif
+                <?php endif; ?>
             </div>
-        @endif
+        <?php endif; ?>
     </div>
-@endif
+<?php endif; ?>
