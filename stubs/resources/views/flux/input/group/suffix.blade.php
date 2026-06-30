453e0319338e79285cb5e9e8a4f5736c9c3fdc02
Maurits <maurits@ympact-technologies.nl>
2026-06-30 22:14:09 +0200

    add size prop on flux:input group and make border radius consistent

--- stubs/resources/views/flux/input/group/suffix.blade.php
+++ stubs/resources/views/flux/input/group/suffix.blade.php
@@ -1,5 +1,9 @@
 @blaze(fold: true)
 
+@props([
+    'size' => null,
+])
+
 @php
 $classes = Flux::classes([
     'flex items-center px-4 text-sm whitespace-nowrap',
@@ -8,7 +12,12 @@ $classes = Flux::classes([
     'border-zinc-200 dark:border-white/10',
     'rounded-e-lg',
     'border-e border-t border-b shadow-xs',
-]);
+])->add(match ($size) {
+    default => 'text-base sm:text-sm rounded-e-lg',
+    'sm' => 'text-sm rounded-e-md',
+    'xs' => 'text-xs rounded-e-sm',
+});
+
 @endphp
 
 <div {{ $attributes->class($classes) }} data-flux-input-group-suffix>
