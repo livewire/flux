d0e4a9988fa457a6b34d7ef327a3c2af4560548c
Caleb Porzio <calebporzio@gmail.com>
2026-08-17 16:49:21 -0400

    Restore navbar action proportions

--- stubs/resources/views/flux/navbar/index.blade.php
+++ stubs/resources/views/flux/navbar/index.blade.php
@@ -7,7 +7,7 @@
 
 @php
 $classes = Flux::classes()
-    ->add('flex items-center gap-1 py-3')
+    ->add('flex items-center gap-0.5 py-3')
     ->add($scrollable ? ['overflow-x-auto overflow-y-hidden'] : [])
     ;
 @endphp
