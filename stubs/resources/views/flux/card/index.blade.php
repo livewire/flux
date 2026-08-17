7f44e37b3fbe3d175857a69923e9b47ddc53d517
Caleb Porzio <calebporzio@gmail.com>
2026-08-17 12:07:06 -0400

    Add soft card variant

--- stubs/resources/views/flux/card/index.blade.php
+++ stubs/resources/views/flux/card/index.blade.php
@@ -1,13 +1,17 @@
 @blaze(fold: true)
 
 @props([
+    'variant' => 'outline',
     'size' => null,
 ])
 
 @php
 $classes = Flux::classes()
-    ->add('[:where(&)]:bg-white dark:[:where(&)]:bg-white/10')
-    ->add('border border-zinc-200 dark:border-white/10')
+    ->add('border')
+    ->add(match ($variant) {
+        'soft' => '[:where(&)]:bg-black/[0.02] border-black/[0.025] dark:[:where(&)]:bg-white/[0.06] dark:border-white/[0.065]',
+        default => '[:where(&)]:bg-white border-zinc-200 dark:[:where(&)]:bg-white/10 dark:border-white/10',
+    })
     ->add(match ($size) {
         default => '[:where(&)]:p-6 [:where(&)]:rounded-xl',
         'sm' => '[:where(&)]:p-4 [:where(&)]:rounded-lg',
