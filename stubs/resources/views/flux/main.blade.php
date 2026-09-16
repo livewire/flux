0d6f5894e0b72cc1cac92e59ed34d84cc2a5e8e4
Caleb Porzio <calebporzio@gmail.com>
2026-09-16 15:49:59 -0400

    Allow inset colors to be customized and simplify inner container

--- stubs/resources/views/flux/main.blade.php
+++ stubs/resources/views/flux/main.blade.php
@@ -8,7 +8,7 @@
 @php
 $classes = Flux::classes('[grid-area:main]')
     ->add($inset ? 'p-6 lg:p-10' : 'p-6 lg:p-8')
-    ->add($inset ? 'min-w-0 min-h-0 bg-white dark:bg-zinc-800 lg:m-2 lg:rounded-xl lg:border lg:border-zinc-200 lg:shadow-xs dark:lg:border-zinc-700' : '')
+    ->add($inset ? 'min-w-0 min-h-0 [:where(&)]:bg-white dark:[:where(&)]:bg-zinc-800 lg:m-2 lg:rounded-xl lg:border lg:[:where(&)]:border-zinc-200 lg:shadow-xs dark:lg:[:where(&)]:border-zinc-700' : '')
     ->add($inset ? 'lg:overflow-y-auto lg:overscroll-contain' : '')
     ->add('[[data-flux-container]_&]:px-0') // If there is a wrapping container, let IT handle the x padding...
     ->add($container && ! $inset ? 'mx-auto w-full [:where(&)]:max-w-7xl' : '')
@@ -17,7 +17,7 @@ $classes = Flux::classes('[grid-area:main]')
 
 <div {{ $attributes->class($classes) }} data-flux-main @if ($inset) data-flux-main-inset @endif>
     <?php if ($container && $inset): ?>
-        <div class="mx-auto w-full [:where(&)]:max-w-7xl" data-flux-container>
+        <div class="mx-auto w-full [:where(&)]:max-w-7xl">
             {{ $slot }}
         </div>
     <?php else: ?>
